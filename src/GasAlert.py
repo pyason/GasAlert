65c8866a5689ae78e0897e44b93999be97e353ca
pyason <paul.yason@gmail.com>
2022-05-03 19:53:31 -0400

    First add for GasAlert.py

---
+++ src/GasAlert.py
@@ -0,0 +1,6 @@
+# This is the GasAlert app to create and send alerts when gas price alert for GTA is issued in twitter
+
+import time
+
+bearer_token="AAAAAAAAAAAAAAAAAAAAANpQcAEAAAAA8KaUxOhVRqOh5xLRX7eMdClBnsI%3DjFlogyFYyJDPqTjw5naTavplumodw8H1Z8J4Tfg74T642bNA7K"
+
