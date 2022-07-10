1b483c02e72ee005585f68676e9e95a722154bc8
pyason <paul.yason@gmail.com>
2022-07-09 20:19:24 -0400

    Add tests folder and add second schedule processing logic

--- src/GasAlert.py
+++ src/GasAlert.py
@@ -7,7 +7,7 @@ from twitterRequest import requestTwitter
 from emailClient import sendEmail
 import logger
 
-def findTodaysUpdate(tweets):
+def findTodaysUpdate(tweets, eventTime):
     isUpdatedToday = False
     emailContent = {
         "title": "GasAlert: ",
@@ -18,11 +18,17 @@ def findTodaysUpdate(tweets):
     createdAt = firstTweet["created_at"]
     tweet = firstTweet["text"]
     tweet = tweet[:tweet.find("\n")]
-    isoCreatedAt = date.fromisoformat(createdAt[:createdAt.find("T")])
-    today = date.today()
-    logger.verboseDebug("Date converted and captured in tweet : ", isoCreatedAt, createdAt)
 
-    if isoCreatedAt == today:
+    # Processing event and tweet's timestamps
+    eventTimeTIndex = eventTime.find("T")
+    createdAtTIndex = createdAt.find("T")
+    eventTimeHour = int(eventTime[eventTimeTIndex+1:eventTimeTIndex+3])
+    createdAtHour = int(createdAt[createdAtTIndex+1:createdAtTIndex+3])
+    isoCreatedAt = date.fromisoformat(createdAt[:createdAtTIndex])
+    today = date.today()
+
+    if isoCreatedAt == today and (eventTimeHour <= 16 or createdAtHour > 16):
+        logger.verboseDebug("Inside extracting tweet", "")
         if tweet.find("increase") != -1:
             emailContent["title"] += tweet[0:1] + " Gas price increase @" + str(today)
             emailContent["message"] = tweet
@@ -34,16 +40,18 @@ def findTodaysUpdate(tweets):
 
     return isUpdatedToday, emailContent
 
-
 def lambda_handler(event, context):
-    print("[INFO] Event handler received event")
+    logger.info("Event handler received event")
+    logger.verboseDebug("Event received: ", event)
     try:
+        eventTime = event["time"]
+        logger.debug("Event time: ", eventTime)
         twitterRes = requestTwitter()
         logger.verboseDebug("Get response from calling twitterRequest: ", twitterRes)
-        isUpdatedToday, emailContent = findTodaysUpdate(twitterRes)
+        isUpdatedToday, emailContent = findTodaysUpdate(twitterRes, eventTime)
         if isUpdatedToday:
             logger.info("Gas price changed, sending email")
             sendEmail(emailContent)
-        else: logger.info("No change in today's gas price")
+        else: logger.info("No change or no tweeter update in today's gas price")
     except:
         logger.error("Calling module exception: ", sys.exc_info())
---
+++ tests/GasAlert_test.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.insert(1, '../src')
+
+from GasAlert import lambda_handler
+
+firstSchedulerEvent = {'id': '2e546d39-1259-fcc8-774e-424ec5179b35', 'detail-type': 'Scheduled Event', 'source': 'aws.events', 'time': '2022-07-09T16:30:00Z', 'region': 'ca-central-1', 'detail': {}}
+secondSchedulerEvent = {'id': '2e546d39-1259-fcc8-774e-424ec5179b35', 'detail-type': 'Scheduled Event', 'source': 'aws.events', 'time': '2022-07-09T22:30:00Z', 'region': 'ca-central-1', 'detail': {}}
+
+# lambda_handler(firstSchedulerEvent,  {})
+lambda_handler(secondSchedulerEvent, {})
